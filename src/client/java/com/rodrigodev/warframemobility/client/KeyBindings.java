d9745b6354c411ade06f40a17213a007b2a03b70
RodrigoDEV <rocapucho123123@gmail.com>
2026-08-04 17:08:37 -0300

    removing unused imports

--- src/client/java/com/rodrigodev/warframemobility/client/KeyBindings.java
+++ src/client/java/com/rodrigodev/warframemobility/client/KeyBindings.java
@@ -1,9 +1,6 @@
 package com.rodrigodev.warframemobility.client;
 
 import org.lwjgl.glfw.GLFW;
-import org.slf4j.LoggerFactory;
-import org.slf4j.Logger;
-
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.KeyMapping;
