6b6808f1f1cf8234938d342aed4fbe9e444393a4
RodrigoDev <onovodash@outlook.com.br>
2026-08-07 10:20:14 -0300

    entity fall damage bug

--- src/client/java/com/rodrigodev/warframemobility/client/mixin/LivingEntityMixin.java
+++ src/client/java/com/rodrigodev/warframemobility/client/mixin/LivingEntityMixin.java
@@ -2,18 +2,17 @@ package com.rodrigodev.warframemobility.client.mixin;
 
 import com.rodrigodev.warframemobility.client.WarframeMobilityState;
 
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin {
-
 
     @Inject(
         method = "causeFallDamage",
@@ -26,6 +25,11 @@ public class LivingEntityMixin {
             DamageSource damageSource,
             CallbackInfoReturnable<Boolean> cir
     ) {
+
+        // Só afeta jogadores
+        if (!((Object) this instanceof Player)) {
+            return;
+        }
 
         if (!WarframeMobilityState.isEnabled()) {
             return;
